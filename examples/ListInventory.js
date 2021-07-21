c776b2baf917c8a8ebee3de020f32daf6888a5a3
Sampli <samplics@gmail.com>
2021-07-21 17:41:30 -0500

    Add Examples

---
+++ examples/ListInventory.js
@@ -0,0 +1,38 @@
+const Skincrib = require('../../src/index.js');
+
+const market = new Skincrib({
+    key: 'YOUR SKINCRIB KEY',
+    reconnect: true
+});
+
+const PERCENT_INCREASE = 15; // items will be listed 15% above their base price.
+
+market.on('authenticated', (message) => {
+    console.log(message);
+
+    market.loadInventory()
+    .then((inventory) => {
+        //only list items that are able to be listed.
+        const items = inventory.filter(item => {
+            return item.accepted && item.tradable;
+        });
+
+        //add a key to each item that defines the percent increase.
+        items.forEach(item => {
+            item.percentIncrease = PERCENT_INCREASE;
+        });
+
+        //list the items.
+        market.createListings(items)
+        .then((listings) => {
+            //show the listings.
+            listings.forEach(listing => {
+                console.log('Created Listing: ', listing);
+            });
+        }, (err) => {
+            console.error(err);
+        });
+    }, (err) => {
+        console.error(err);
+    });
+});
